21703a9416b0f9690b98e1d48ad390afb450815c
Sreerag PA <catchsreerag@gmail.com>
2024-08-02 15:26:47 +0530

    Google Signup Bug Fixed

--- src/frameworks/utils/googleAuthVerifyToken.ts
+++ src/frameworks/utils/googleAuthVerifyToken.ts
@@ -1,19 +1,12 @@
-import { OAuth2Client } from 'google-auth-library';
-
-interface IdTokenPayload {
-  // Define the structure of your ID token payload here
-  sub: string;
-  email: string;
-  // ... other properties
-}
+import { LoginTicket, OAuth2Client, TokenPayload } from 'google-auth-library';
 
 const clientId = process.env.GOOGLE_CLIENT_ID;
 
-async function verifyIdToken(idToken: string): Promise<IdTokenPayload> {
+async function verifyIdToken(idToken: string): Promise<TokenPayload> {
   try {
     const client = new OAuth2Client(clientId);
-    const ticket = await client.verifyIdToken({ idToken, audience: clientId });
-    const payload = ticket.getPayload()   as IdTokenPayload; // Type assertion
+    const ticket: LoginTicket = await client.verifyIdToken({ idToken, audience: clientId });
+    const payload: TokenPayload = ticket.getPayload() as TokenPayload; // Type assertion
     return payload;
   } catch (error) {
     console.error(error);
